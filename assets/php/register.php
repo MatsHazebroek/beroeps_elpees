4d268bfa158fdb8510ed2f0659b3f1ff68db3f11
Mats Hazebroek <91888302+MatsHazebroek@users.noreply.github.com>
2022-10-04 13:50:06 +0200

    k

--- assets/php/register.php
+++ assets/php/register.php
@@ -1,57 +1,42 @@
+<?php include('functions.php') ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<title>Document</title>
 </head>
 <body>
-
-<?php
-    require('config.php');
-    if (isset($_REQUEST['username'])) {
-        // Haalt backslashes weg
-        $username = stripslashes($_REQUEST['username']);
-        // Haalt speciale characters uit de string
-        $username = mysqli_real_escape_string($mysqli, $username);
-        $email    = stripslashes($_REQUEST['email']);
-        $email    = mysqli_real_escape_string($mysqli, $email);
-        $password = stripslashes($_REQUEST['password']);
-        $password = mysqli_real_escape_string($mysqli, $password);
-        $create_datetime = date("Y-m-d H:i:s");
-        // Hier voert hij de query uit en voegt de ingevoerde informatie in de table van de database
-        $query    = "INSERT into `users` (username, password, email, create_datetime) VALUES 
-            ('$username', '" . md5($password) . "', '$email', '$create_datetime')";
-
-        $result   = mysqli_query($mysqli, $query);
-
-        if ($result) {
-            echo "<div class='form'>
-                  <h3>You are registered successfully.</h3><br/>
-                  <p class='link'>Klik hier om in te <a href='login.php'>loggen</a></p>
-                  </div>";
-        } else {
-            echo "<div class='form'>
-                  <h3>Required fields are missing.</h3><br/>
-                  <p class='link'>Klik hier om te <a href='register.php'>Registreren</a> again.</p>
-                  </div>";
-        }
-    } else {
-
-?>
-
-    <form class="form" action="" method="post">
-        <h1 class="login-title">Registreer</h1>
-        <input type="text" class="login-input" name="username" pattern="[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$" placeholder="Gebruikersnaam" required />
-        <input type="text" class="login-input" name="email" pattern="[A-Za-z0-9._+-]+@[A-Za-z0-9 -]+\.[a-z]{2,}" placeholder="E-mail" required>
-        <input type="password" class="login-input" name="password" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" placeholder="Wachtwoord" required>
-        <input type="submit" name="submit" value="Register" class="login-button">
-        <p class="link">Heb je al een account? <a href="login.php">Login hier</a></p>
-    </form>
-<?php
-    }
-?>
-    
+	<div class="header">
+	<h2>Register</h2>
+</div>
+<form method="post" action="register.php">
+	<?php echo display_error(); ?>
+	<div class="input-group">
+		<label>Username</label>
+		<input type="text" name="username" value="<?php echo $username;?>">
+	</div>
+	<div class="input-group">
+		<label>Email</label>
+		<input type="email" name="email" value="<?php echo $email;?>">
+	</div>
+	<div class="input-group">
+		<label>Password</label>
+		<input type="password" name="password_1">
+	</div>
+	<div class="input-group">
+		<label>Confirm password</label>
+		<input type="password" name="password_2">
+	</div>
+	<div class="input-group">
+		<button type="submit" class="btn" name="register_btn">Register</button>
+	</div>
+	<p>
+		Already a member? <a href="login.php">Sign in</a>
+	</p>
+</form>
 </body>
 </html>
