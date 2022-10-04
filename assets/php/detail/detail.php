379e29948d99ded3d520d99436381ea997fff7a3
joepdej <joep.dejong2003@gmail.com>
2022-10-04 16:10:10 +0200

    1

--- assets/php/detail/detail.php
+++ assets/php/detail/detail.php
@@ -1,3 +1,19 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-$db = mysqli_connect('localhost', 'DBgebruiker', 'DBgebruiker', 'VerzamelDB');
+$db = mysqli_connect('localhost', 'DBgebruiker', 'DBgebruiker', 'BeroepsDB');
+
+$sql = "select * from `VerzamelDB`";
+$result = mysqli_query($db, $sql) or die("Error in Selecting " . mysqli_error($db));
+
+$emparray = array();
+while($row =mysqli_fetch_assoc($result))
+{
+    $emparray[] = $row;
+}
+
+echo json_encode($emparray);
+
+?>
