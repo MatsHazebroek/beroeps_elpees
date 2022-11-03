6dc2b548c75a94253c6dc3def1e40febbb910d32
joepdej <joep.dejong2003@gmail.com>
2022-11-03 09:21:48 +0100

    1

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -141,7 +141,7 @@ function login(){
 				$_SESSION['user'] = $logged_in_user;
 				$_SESSION['success']  = "You are now logged in";
 
-				header('location: index.php');
+				header('location:../../index.php');
 			}
 		}else {
 			array_push($errors, "Wrong username/password combination");
