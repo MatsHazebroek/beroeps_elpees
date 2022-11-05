1b96ddc7db1edefa8b7cb0fc2ad9149c24648800
Mats Hazebroek <91888302+MatsHazebroek@users.noreply.github.com>
2022-11-05 20:34:15 +0100

    live version

--- detail.php
+++ detail.php
@@ -45,33 +45,12 @@ if (mysqli_num_rows($result)) {
 
                     <div class="mySlides fade">
                         <div class="numbertext">1 / 3</div>
-                        <img src="assets/php/upload/<?php echo $item['ItemImage']?>"style="width:100%">
+                        <img src="./assets/php/upload/<?php echo $item['ItemImage']?>"style="width:100%">
                         <div class="text">Caption Text</div>
                     </div>
-
-                    <div class="mySlides fade">
-                        <div class="numbertext">2 / 3</div>
-                        <img src="<?php echo $item['ItemImage']?>" style="width:100%">
-                        <div class="text">Caption Two</div>
-                    </div>
-
-                    <div class="mySlides fade">
-                        <div class="numbertext">3 / 3</div>
-                        <img src="<?php echo $item['ItemImage']?>" style="width:100%">
-                        <div class="text">Caption Three</div>
-                    </div>
-
-                    <a class="prev" onclick="plusSlides(-1)">❮</a>
-                    <a class="next" onclick="plusSlides(1)">❯</a>
-
                 </div>
                 <br>
-
-                <div style="text-align:center">
-                    <span class="dot" onclick="currentSlide(1)"></span>
-                    <span class="dot" onclick="currentSlide(2)"></span>
-                    <span class="dot" onclick="currentSlide(3)"></span>
-                </div>
+                
                 <div class="buttons">
                     <button id="edit"><a href="./assets/php/detail/edit_item.php?id=<?php echo $item['Id']?>">Bewerk</a></button>
                     <button id="delete"><a href="./assets/php/verwijder.php?id=<?php echo $item['Id']?>">Verwijder</a></button>
