f8ec4710466677a034e67846a9dcba5f7b8a8c9d
Mats Hazebroek <91888302+MatsHazebroek@users.noreply.github.com>
2022-11-07 09:53:29 +0100

    weer silvan zn bugs moeten fixen

--- overzicht.php
+++ overzicht.php
@@ -145,20 +145,19 @@
                     while($item = mysqli_fetch_assoc($result)) {
                         ?>
                         <a href="detail.php?id=<?php echo $item['Id']?>" class="card">
-                            <img src="https://87957.stu.sd-lab.nl/stock.jpeg" class="card__image" alt=""/>
+                            <img src="assets/php/upload/<?php echo $item['ItemImage']?>" class="card__image" alt="" />
                             <div class="card__overlay">
                                 <div class="card__header">
                                     <svg class="card__arc" xmlns="http://www.w3.org/2000/svg%22%3E"><path/></svg>
                                     <div class="card__header-text">
                                         <h1 class="card__title"><?php echo $item['NaamItem']?></h1>
                                         <span class="card__status"><?php echo $item['Artiest']?><br></span>
-                                        <!-- <span class="card__status"><?php echo $item['ReleaseDatum']?><br></span>
-                                        <span class="card__status"><?php echo $item['Genre']?><br></span>
-                                        <span class="card__status"><?php echo $item['Formaat']?></span>
-                                        <span class="card__status"><?php echo $item['Artiest']?></span> -->
                                     </div>
                                 </div>
-                                <p class="card__description">Omschrijving:<br><?php echo $item['Omschrijving']?></p>
+                                <span class="card__description"><span>Releasedatum:</span> <?php echo $item['ReleaseDatum']?><br></span>
+                                <span class="card__description"><span>Genre:</span> <?php echo $item['Genre']?><br></span>
+                                <span class="card__description"><span>Formaat:</span> <?php echo $item['Formaat']?></span>
+                                <p class="card__description"><span>Omschrijving:</span><br><?php echo $item['Omschrijving']?></p>
                             </div>
                         </a>
                     <?php 
