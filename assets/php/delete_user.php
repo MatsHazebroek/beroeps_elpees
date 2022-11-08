175fa4de1713e1ea8af448f4be1382c6d31d5e8f
Mats Hazebroek <91888302+MatsHazebroek@users.noreply.github.com>
2022-11-08 10:55:06 +0100

    s

--- assets/php/delete_user.php
+++ assets/php/delete_user.php
@@ -3,14 +3,19 @@ include('./functions.php');
 
 $id = $_GET['id'];
 
-$queryUser = "DELETE * FROM `multi_login` WHERE id = " . $id;
+$queryUser = "DELETE FROM `multi_login` WHERE id = " . $id;
 
-$queryItem = "DELETE * FROM `VerzamelDB` WHERE user = " . $id;
+$queryItem = "DELETE FROM `VerzamelDB` WHERE user = " . $id;
 
 $result = mysqli_query($db, $queryUser);
 
-$result = mysqli_query($db, $queryItem);
+$result2 = mysqli_query($db, $queryItem);
 
-header('location:./home.php')
+if ($result && $result2) {
+    header('location:./home.php');
+} else {
+    echo mysqli_error($db);
+}
+
     
 ?>
