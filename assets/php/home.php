3560d1fbaeea75bb6252d2d22134110a5bdd3482
Mats Hazebroek <91888302+MatsHazebroek@users.noreply.github.com>
2022-11-08 10:43:17 +0100

    s

--- assets/php/home.php
+++ assets/php/home.php
@@ -16,12 +16,7 @@ if (mysqli_num_rows($result) > 0) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../css/navBar.css">
-        <style>
-            table, th, td {
-                margin-top: 500px;
-                border: 1px solid;
-            }
-        </style>
+        <link rel="stylesheet" href="../css/home.css">
         <title>Admin page</title>
     </head>
     <body>
@@ -30,7 +25,6 @@ if (mysqli_num_rows($result) > 0) {
 
             <nav class="navbar">
             <ul>
-                <!-- <li><a href="../../index.php">Home</a></li> -->
                 <li><a href="../../overzicht.php">Overzicht</a></li>
                 <li><a href="#">Admin</a></li>
                 <li><a href="./logout.php">Log uit</a></li>
@@ -38,31 +32,33 @@ if (mysqli_num_rows($result) > 0) {
             </nav>
         </header>
 
-
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>E-mail</th>
-                <th>User Type</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
-            <?php 
-            while($item = mysqli_fetch_assoc($result)) {
+        <div class="table">
+            <table>
+                <tr id="fixed">
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>E-mail</th>
+                    <th>User Type</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <?php 
+                while($item = mysqli_fetch_assoc($result)) {
+                    ?>
+                        <tr>
+                            <th><?php echo $item['id']; ?></th>
+                            <th><?php echo $item['username']; ?></th>
+                            <th><?php echo $item['email']; ?></th>
+                            <th><?php echo $item['user_type']; ?></th>
+                            <th><a id="edit" href="./detail/edit_user.php?id=<?php echo $item['id'];?>">Verander</a></th>
+                            <th><a id="delete" href="./delete_user.php?id=<?php echo $item['id']; ?>">Verwijder</a></th>
+                        </tr>
+                    <?php
+                }
                 ?>
-                    <tr>
-                        <th><?php echo $item['id']; ?></th>
-                        <th><?php echo $item['username']; ?></th>
-                        <th><?php echo $item['email']; ?></th>
-                        <th><?php echo $item['user_type']; ?></th>
-                        <th><a href="./detail/edit_user.php?id=<?php echo $item['id'];?>">Verander</a></th>
-                        <th><a href="./delete_user.php?id=<?php echo $item['id']; ?>">Verwijder</a></th>
-                    </tr>
-                <?php
-            }
-            ?>
-        </table>
+            </table>
+        </div>
+
         
     </body>
     </html>
